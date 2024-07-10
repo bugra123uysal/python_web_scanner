14ab3032d612923828e996e9e3e056e71e86099d
buğra uysal <uysalbugra134@gmail.com>
2024-07-10 04:12:16 +0300

    Started scanning images, downloading them will be implemented later

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
+""" yazı alma  """
 def arst():
   
     
@@ -20,3 +20,19 @@ def arst():
 
 
 arst()
+
+def gor():
+   uurl='https://tr.wikipedia.org/wiki/At'
+   a=requests.get(uurl)
+   b=a.content
+   c=BeautifulSoup(b,'html.parser')
+
+   w=c.find_all('img')
+   imgurl=[img['src']for img in w if 'src' in img.attrs]
+
+   print(imgurl)
+
+   
+gor()   
+
+
